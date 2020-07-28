7b88d0bc871e1a323358fec8ecce490607cb5196
Roman Vakulenko <vakulka95@gmail.com>
2020-07-29 00:38:20 +0300

    I modified several templates, added color for status, cleared script and updated css file and edited Login html

--- static/v3/styles/script/design__script.js
+++ static/v3/styles/script/design__script.js
@@ -1,7 +1,3 @@
-const eyeClose = document.querySelector('.eye-close');
-const eyeOpen = document.querySelector('.eye-open');
-const signInPass = document.getElementById('pass');
-
 
 // Show and hide modal
 const filter_btn = document.querySelector('.filter .filter-btn');
@@ -9,31 +5,42 @@ const close_btn = document.querySelector('.close');
 const modal = document.querySelector('.modal');
 const body = document.querySelector('body');
 const user = document.querySelector('.user');
-const userImg = document.querySelector('.user img');
+// const userImg = document.querySelector('.user img');
 const menuModal = document.querySelector('.menu-modal');
-const drag = document.querySelector('.drag');
-const dragTitle = document.querySelector('.drag-title');
-const filter = document.querySelector('.filter');
-const filterDesk = document.querySelector('.filter-desktop');
-const appClients = document.querySelector('.app__clients');
-const psychologists = document.querySelector('.psychologists');
-const header = document.querySelector('.header');
-const main = document.querySelector('.main');
-const rate = document.querySelector('.rate');
-const feedbacks = document.querySelector('.feedbacks');
+// const drag = document.querySelector('.drag');
+// const dragTitle = document.querySelector('.drag-title');
+// const filter = document.querySelector('.filter');
+// const filterDesk = document.querySelector('.filter-desktop');
+// const appClients = document.querySelector('.app__clients');
+// const psychologists = document.querySelector('.psychologists');
+// const header = document.querySelector('.header');
+// const main = document.querySelector('.main');
+// const rate = document.querySelector('.rate');
+// const feedbacks = document.querySelector('.feedbacks');
 const menuLink= document.querySelectorAll('.menu--link');
 const tablesTr = document.querySelectorAll('table tr');
-const titleArrow = document.querySelector('.title__arrow');
-const button = document.querySelector('.button');
-const desktopNav = document.querySelector('.desktop-nav')
-const gender = document.querySelector('.app-gender-form');
-const form = document.querySelector('.form');
-const arrow = document.querySelector('.arrow');
+// const titleArrow = document.querySelector('.title__arrow');
+// const button = document.querySelector('.button');
+// const desktopNav = document.querySelector('.desktop-nav')
+// const gender = document.querySelector('.app-gender-form');
+// const form = document.querySelector('.form');
+// const arrow = document.querySelector('.arrow');
 const filterSearch = document.querySelector('.filter-desktop-search');
 const nameInput = document.querySelectorAll('.name');
-const phoneInput = document.querySelectorAll('.phone');
+// const phoneInput = document.querySelectorAll('.phone');
 const filterMobSearch = document.querySelector('.filter-mob-input');
 const nameMob = document.querySelectorAll('.name-mob');
+const eyeClose = document.querySelector('.eye-close');
+const eyeOpen = document.querySelector('.eye-open');
+const signInPass = document.getElementById('pass');
+
+// show modal menu
+user.onclick = () => {
+    menuModal.classList.toggle('show');
+    menuModal.onclick = () => { 
+    menuModal.classList.remove('show');
+    }
+}
 
 
 
@@ -61,41 +68,6 @@ menuLink.forEach(el => {
         }
     }
 })
-
-// Hide elements
-
-
-
-function hideFilterBlock(){
-    if(document.body.clientWidth >= 1024){
-        filter.style.display = 'none';
-        filterDesk.style.display = 'block';
-        appClients.style.display = 'none';
-        header.style.position = 'relative';
-        main.style.display = 'block';
-        userImg.style.display = 'none';
-        user.style.display = 'none';
-        desktopNav.style.display = 'block';
-        psychologists.style.display = 'none';
-        feedbacks.style.display = 'none';
-
-    }else{
-        filter.style.display = 'block';
-        filterDesk.style.display = 'none';
-        appClients.style.display = 'block';
-        header.style.position = 'sticky';
-        main.style.display = 'none';
-        userImg.style.display = 'block';
-        desktopNav.style.display = 'none';
-        psychologists.style.display = 'block';
-        feedbacks.style.display = 'block';
-
-
-    }
-}
-hideFilterBlock();
-
-window.addEventListener('resize', hideFilterBlock);
 
 // Background table
 
@@ -125,93 +97,23 @@ filterSearch.oninput = function(){
                 el.parentElement.parentElement.classList.remove('hide');
             }
         });
-        phoneInput.forEach(el => {
-            if(el.innerText.search(val) == -1 ){
-                el.parentElement.parentElement.classList.add('hide')
-            }
-            else{
-                el.parentElement.parentElement.classList.remove('hide');
-            }
-        });
+        // phoneInput.forEach(el => {
+        //     if(el.innerText.search(val) == -1 ){
+        //         el.parentElement.parentElement.classList.add('hide')
+        //     }
+        //     else{
+        //         el.parentElement.parentElement.classList.remove('hide');
+        //     }
+        // });
     }else{
         nameInput.forEach(el => {
             el.parentElement.parentElement.classList.remove('hide')
         });
-        phoneInput.forEach(el => {
-            el.parentElement.parentElement.classList.remove('hide')
-        });
+        // phoneInput.forEach(el => {
+        //     el.parentElement.parentElement.classList.remove('hide')
+        // });
     }
 }
-
-
-
-
-// change Arrow
-
-function changeArrow(){
-    if(document.body.clientWidth >= 1024){
-        form.style.width = '75%';
-        userImg.style.display = 'none';
-        user.style.display = 'none';
-        desktopNav.style.display = 'flex';
-        gender.style.display = 'none';
-
-    }else{
-        form.style.width = '100%';
-        userImg.style.display = 'block';
-        user.style.display = 'block';
-        desktopNav.style.display = 'none';
-        gender.style.display = 'block';
-
-
-
-    }
-}
-changeArrow();
-
-window.addEventListener('resize', changeArrow); 
-
-
-
-
-// show modal menu
-user.onclick = () => {
-
-    menuModal.classList.toggle('show');
-    menuModal.onclick = () => { 
-    menuModal.classList.remove('show');
-    }
-}
-
-
-
-function hideBlock(){
-    if(document.body.clientWidth >= 1024){
-        drag.classList.add('d-flex');
-        dragTitle.style.display = 'block';
-        user.style.display = 'none';
-        userImg.style.display = 'none';
-        desktopNav.style.display = 'block';
-    }else{
-        drag.classList.remove('d-flex')
-        drag.style.display = 'none';
-        dragTitle.style.display = 'none';
-        user.style.display = 'block';
-        userImg.style.display = 'block';
-        desktopNav.style.display = 'none';
-
-    }
-}
-hideBlock();
-
-window.addEventListener('resize', hideBlock);
-
-
-// Edit inputs
-
-
-
-
 
 // Search application in search input
 
@@ -265,47 +167,118 @@ function onSubmitLogin(token) {
     document.getElementById('reCaptchaForm').submit();
 }
 
-const eyeClose = document.querySelector('.eye-close');
-const eyeOpen = document.querySelector('.eye-open');
-const signInPass = document.getElementById('pass');
-
-function showMePassword() {
-    eyeClose.onclick = function(){
-        eyeClose.style.display = 'none';
-        eyeOpen.style.display = 'block';
-        signInPass.setAttribute('type', 'text');
-
-        eyeOpen.onclick = function(){
-            eyeClose.style.display = 'block';
-            eyeOpen.style.display = 'none';
-            signInPass.setAttribute('type', 'password');
-
-        }
-    }
-
-}
-
 $(document).ready(function($) {
     $(".clickable-row").click(function() {
         window.document.location = $(this).data("href");
     });
 });
 
-function showMePassword() {
-    eyeClose.onclick = function(){
-        eyeClose.style.display = 'none';
-        eyeOpen.style.display = 'block';
-        signInPass.setAttribute('type', 'text');
 
+    eyeClose.onclick = function(){
+        if( !eyeClose.classList.contains('hide')){
+            eyeClose.classList.add('hide');
+            eyeOpen.classList.remove('hide')
+            signInPass.setAttribute('type', 'text');
+        }
         eyeOpen.onclick = function(){
-            eyeClose.style.display = 'block';
-            eyeOpen.style.display = 'none';
+            eyeClose.classList.remove('hide');
+            eyeOpen.classList.add('hide')
             signInPass.setAttribute('type', 'password');
 
         }
     }
 
-}
 
-showMePassword();
 
+
+
+
+
+
+    
+
+
+// change Arrow
+
+// function changeArrow(){
+//     if(document.body.clientWidth >= 1024){
+//         form.style.width = '75%';
+//         userImg.style.display = 'none';
+//         user.style.display = 'none';
+//         desktopNav.style.display = 'flex';
+//         gender.style.display = 'none';
+
+//     }else{
+//         form.style.width = '100%';
+//         userImg.style.display = 'block';
+//         user.style.display = 'block';
+//         desktopNav.style.display = 'none';
+//         gender.style.display = 'block';
+//     }
+// }
+// changeArrow();
+
+// window.addEventListener('resize', changeArrow); 
+
+
+
+
+
+
+// function hideBlock(){
+//     if(document.body.clientWidth >= 1024){
+//         drag.classList.add('d-flex');
+//         dragTitle.style.display = 'block';
+//         user.style.display = 'none';
+//         userImg.style.display = 'none';
+//         desktopNav.style.display = 'block';
+//     }else{
+//         drag.classList.remove('d-flex')
+//         drag.style.display = 'none';
+//         dragTitle.style.display = 'none';
+//         user.style.display = 'block';
+//         userImg.style.display = 'block';
+//         desktopNav.style.display = 'none';
+
+//     }
+// }
+// hideBlock();
+
+// window.addEventListener('resize', hideBlock);
+
+
+// Edit inputs
+
+
+// Hide elements
+
+
+
+// function hideFilterBlock(){
+//     if(document.body.clientWidth >= 1024){
+//         filter.style.display = 'none';
+//         filterDesk.style.display = 'block';
+//         appClients.style.display = 'none';
+//         header.style.position = 'relative';
+//         main.style.display = 'block';
+//         userImg.style.display = 'none';
+//         user.style.display = 'none';
+//         desktopNav.style.display = 'block';
+//         psychologists.style.display = 'none';
+//         feedbacks.style.display = 'none';
+
+//     }else{
+//         filter.style.display = 'block';
+//         filterDesk.style.display = 'none';
+//         appClients.style.display = 'block';
+//         header.style.position = 'sticky';
+//         main.style.display = 'none';
+//         userImg.style.display = 'block';
+//         desktopNav.style.display = 'none';
+//         psychologists.style.display = 'block';
+//         feedbacks.style.display = 'block';
+//     }
+// }
+// hideFilterBlock();
+
+// window.addEventListener('resize', hideFilterBlock);
